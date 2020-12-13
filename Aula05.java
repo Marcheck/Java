9523ed0067ff39089d12c3ce5ade45135fb4e713
Marcheck <63622971+Marcheck@users.noreply.github.com>
2020-12-13 07:52:11 -0300

    Add files via upload

---
+++ Aula05.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula05;
+public class Aula05 {
+   public static void main(String []args){
+       System.out.println("Olá Mundo!");
+   } 
+}
